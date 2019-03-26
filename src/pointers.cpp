b648de054528b345cbea6c48c55a6082b6a74136
SnAzZyC00kie <SnAzZyC00kie@users.noreply.github.com>
2019-03-25 22:48:03 -0400

    Pointers!

--- src/pointers.cpp
+++ src/pointers.cpp
@@ -2,27 +2,35 @@
 
 using namespace std;
 
-void myswap(int * ptr1, int* ptr2)
+void myswap(int * ptr1, int * ptr2)
 {
-  auto temp = ptr1;
-  ptr1 = ptr2;
-  ptr2 = temp;
+  auto temp = *ptr1;
+  *ptr1 = *ptr2;
+  *ptr2 = temp;
+  cout << "The value of ptr1 is" <<ptr1 <<endl;
+  cout << "The value of ptr2 is" <<ptr2 <<endl;
+  
+  cout << "The value of ptr1 is" <<*ptr1 <<endl;
+  cout << "The value of ptr2 is" <<*ptr2 <<endl;
+  
+  
 }
 
 
 int main()
 {
+	
   int a = 25, b = 11;    
-
   cout<<"What am I doing wrong ☹ \n\n";
 
   cout<<"a = "<<a<<", b = "<<b<<endl;
 
 //swap(a,b); //why does theirs work????   
  
-  myswap(a, b); // but mine doesn't ?!?!?!?!!!???
+  myswap(&a,&b); // but mine doesn't ?!?!?!?!!!???
 
   cout<<"a = "<<a<<", b = "<<b<<endl;
+  
 
 
 }
